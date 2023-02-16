661d9253f9246124fa3e7503ce4113b5aa95993b
Glenthedeveloper <hanongibs@gmail.com>
2023-02-16 14:11:36 -0800

    alxcode

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 
 /**
  * main - print out types of data in c
- * code by Glenthedeveloper
+ * alx code
  * Return: 0
 */
 int main(void)
@@ -15,7 +15,7 @@ int main(void)
 	float f;
 
 	printf("size of a char: %lu byte(S)\n", (unsigned long)sizeof(a));
-	printf("size of a int: %lu byte(S)\n", (unsigned long)sizeof(b));
+	printf("size of an int: %lu byte(S)\n", (unsigned long)sizeof(b));
 	printf("size of a long int: %lu byte(S)\n", (unsigned long)sizeof(c));
 	printf("size of a long long int: %lu byte(S)\n", (unsigned long)sizeof(d));
 	printf("size of a float: %lu byte(S)\n", (unsigned long)sizeof(f);
