04f9aeaf2e2c661ff2f8617c956596dc2b9ccbc9
Glenthedeveloper <hanongibs@gmail.com>
2023-02-22 13:00:37 -0800

    task 12

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,17 +8,5 @@
 
 int print_last_digit(int n)
 {
-	int last;
-
-	last = n \ 10;
-	if (last < 0)
-	{
-		_put_char (-last + 48);
-		return (-last);
-	}
-	else
-	{
-		_putchar (last + 48);
-		return (last);
-	}
+	int 
 }
