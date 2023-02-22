8c92d0a6bd576e7070af5888c70ae041590abdb3
Glenthedeveloper <hanongibs@gmail.com>
2023-02-22 14:01:46 -0800

    task7 before2nddeadline

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,11 +2,20 @@
 
 /**
  * print_last_digit - Prints the last digit of a number
- * @n: the number
+ * @n: is the integer to be used for the augment of the function
  * Return: Value of the last digit
  */
 
 int print_last_digit(int n)
 {
-	int 
+	int last;
+
+	last = n % 10;
+
+	if (last < 0)
+		last = last * -1;
+
+	_putchar(last + '0');
+
+	return (last);
 }
