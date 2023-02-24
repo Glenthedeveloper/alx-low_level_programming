f78982d21e2048461d06980e957c147fc57c810a
Glenthedeveloper <hanongibs@gmail.com>
2023-02-24 15:50:25 -0800

    lastquiz

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,8 +1,10 @@
-#include "holberton.h"
+#include "main.h"
+#include <stdio.h>
 
 /**
  * print_number - prints an integer
  * @n:.input integer parameter
+ * Return: 0
  */
 
 void print_number(int n)
