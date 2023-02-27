93b29f15dfda57fb1efbd4bb2051114ec346bf2f
Glenthedeveloper <hanongibs@gmail.com>
2023-02-27 04:53:56 -0800

    putch

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,21 +2,23 @@
 
 /**
  * puts2 - prints every other character of a string
- * @str: the string to print
+ * @str: the string to treated
+ * Return: Always 0
  */
 
 void puts2(char *str)
 {
 	int i;
-	int j;
+	int j = 0;
 
-	for (i = 0 ; str[i] != '\0' ; i++)
+	while (str[j] != '\0')
 	{
-		;
+	j++;
 	}
-	for (j = 0 ; j < i ; j = j + 2)
+
+	for (i = 0; i < j; i += 2)
 	{
-		putchar(str[j]);
+	putchar(str[i]);
 	}
 	putchar('\n');
 }
