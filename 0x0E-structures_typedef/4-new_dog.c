8499a99dce15ef2416b0fcb7111e7cbf1d0bd49b
Glenthedeveloper <hanongibs@gmail.com>
2023-03-20 12:40:48 -0700

    new dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,35 +1,74 @@
-#include "dog.h"
 #include <stdlib.h>
+#include "dog.h"
+char *_strdup(char *str);
 
 /**
- * new_dog - entry point
- * @name: string from main, name of pet
- * @age: number from main, age of pet
- * @owner: string from main, owner of pet
- * Return: p
+ * new_dog - function with 3 arguments
+ * @name: char type pointer
+ * @age: float type
+ * @owner: char type pointer
+ *
+ * Description: creates a new dog
+ * Return: NULL if fail or pointer
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *p;
-	/* reserving memory to struct*/
-	p = malloc(sizeof(dog_t));
-	if (p == NULL)
+	dog_t *new_d;
+
+	new_d = malloc(sizeof(dog_t));
+	if (new_d == NULL)
 		return (NULL);
-	/* Cpunting name pointer*/
-	if (name == NULL)
+
+	new_d->name = _strdup(name);
+	if (!new_d->name)
 	{
-		free(p);
-		free(owner);
+		free(new_d);
 		return (NULL);
 	}
-	if (owner == NULL)
+	new_d->age = age;
+	new_d->owner = _strdup(owner);
+	if (!new_d->owner)
 	{
-		free(p);
-		free(name);
+		free(new_d->name);
+		free(new_d);
 		return (NULL);
 	}
-	p->name = name;
-	p->age = age;
-	p->owner = owner;
-	return (p);
+
+	return (new_d);
+
+}
+
+/**
+ * *_strdup - function with one argument
+ * @str: string argument
+ *
+ * Description: returns a pointer to allocated space in memory
+ * Return: pointer
+ */
+char *_strdup(char *str)
+{
+	int i, j;
+	char *ptr;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		i++;
+	}
+
+	ptr = malloc(sizeof(char) * i + 1);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	j = 0;
+	while (str[j] != '\0')
+	{
+		ptr[j] = str[j];
+		j++;
+	}
+	ptr[j] = '\0';
+	return (ptr);
 }
